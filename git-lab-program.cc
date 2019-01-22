c0477028eaa59f8b6c408e5d4b289ec265a6e382
Sam Adams <sa324515@ohio.edu>
2019-01-22 18:59:54 -0500

    git-lab-program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Sam Adams
+ *        Date: 01/22/2019
+ * Description: CS 2400 Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
